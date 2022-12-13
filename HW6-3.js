3b28933f0125dc0176dda7badde809a0eff5f35d
DmytroHeavy <106928226+DmytroHeavy@users.noreply.github.com>
2022-12-13 16:58:29 +0200

    Add files via upload

---
+++ HW6-3.js
@@ -0,0 +1,5 @@
+const lengthSideSquare = 'Якa довжина однієї сторони квадрата в СМ';
+const lengthtCM = prompt(lengthSideSquare);
+const perimeterCM = lengthtCM * 4;
+const answer = `Периметр квадрата - ${perimeterCM}см`;
+alert(answer)
